1092634e718b7c0c3d04957090bd1fbcbacbed7b
Vladislav Poznyak <vladislav.poznyak@skywindgroup.com>
2018-01-27 15:57:37 +0300

    Update hook into admin panel
    It brokes saving

--- public/scripts/hooks.js
+++ public/scripts/hooks.js
@@ -1,18 +1,18 @@
-const locale = {
-  'Search': 'Поиск',
-  'Create': 'Создать',
-  'Upload Image': 'Загрузить изображение',
-  'Upload Images': 'Загрузить изображения',
-  'Save': 'Сохранить',
-  'reset': 'Сбросить',
-  'delete': 'Удалить',
-  'Select...': 'Выбрать...',
-};
-
 setInterval(() => {
+  const translation = {
+    'Search': 'Поиск',
+    'Create': 'Создать',
+    'Upload Image': 'Загрузить изображение',
+    'Upload Images': 'Загрузить изображения',
+    // 'Save': 'Сохранить',
+    'reset': 'Сбросить',
+    'delete': 'Удалить',
+    'Select...': 'Выбрать...',
+  };
+
   document.querySelectorAll('button, .Select-placeholder').forEach(element => {
-    if(locale[element.textContent]) {
-        element.textContent = locale[element.textContent];
+    if(translation[element.textContent]) {
+        element.textContent = translation[element.textContent];
     }
   });
 }, 1000);
