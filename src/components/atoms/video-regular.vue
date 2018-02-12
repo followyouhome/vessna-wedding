06156e932f5d8defc3990be5c6727d0abaa2315d
Vladislav Poznyak <vladislav.poznyak@skywindgroup.com>
2018-02-12 18:27:01 +0300

    Add poster as background

--- src/components/atoms/video-regular.vue
+++ src/components/atoms/video-regular.vue
@@ -1,10 +1,12 @@
 <template>
-  <figure class="video-regular">
-  	<video class="video-regular__video" autoplay loop muted ref="video" v-bind:poster="poster" v-if="webm.url" v-bind:src="webm.url">
-  		<source v-if="webm.url" v-bind:src="webm.url" type='video/webm; codecs="vp8, vorbis"'>
-  		<source v-if="mp4.url" v-bind:src="mp4.url" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
-  		<source v-if="ogv.url" v-bind:src="ogv.url" type='video/ogg; codecs="theora, vorbis"'>
-  	</video>
+  <figure class="video-regular" :style="`background-image: url(${poster});`">
+    <no-ssr>
+    	<video class="video-regular__video" autoplay loop muted preload="none" ref="video" v-if="webm.url" v-bind:src="webm.url">
+    		<source v-if="webm.url" v-bind:src="webm.url" type='video/webm; codecs="vp8, vorbis"'>
+    		<source v-if="mp4.url" v-bind:src="mp4.url" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+    		<source v-if="ogv.url" v-bind:src="ogv.url" type='video/ogg; codecs="theora, vorbis"'>
+    	</video>
+    </no-ssr>
   </figure>
 </template>
 
@@ -41,5 +43,16 @@
 </script>
 
 <style lang="scss">
+  @keyframes show {
+    from {opacity: 0; }
+    to { opacity: 1; }
+  }
 
+  .video-regular {
+    background: transparent 50% 50% / cover no-repeat;
+  }
+
+  video {
+    animation: show 0.5s forwards;
+  }
 </style>
