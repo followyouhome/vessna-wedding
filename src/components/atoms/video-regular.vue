abc1cfe11fdb7b762d19059e7ae536b1d0181274
Vladislav Poznyak <vladislav.poznyak@skywindgroup.com>
2018-02-13 16:42:22 +0300

    Add resize for video poster

--- src/components/atoms/video-regular.vue
+++ src/components/atoms/video-regular.vue
@@ -1,5 +1,5 @@
 <template>
-  <figure class="video-regular" :style="`background-image: url(${poster});`">
+  <figure class="video-regular" :style="`background-image: url(${url});`">
   	<video class="video-regular__video" autoplay loop muted preload="none" ref="video" v-show="show" v-bind:src="webm.url">
   		<source v-if="webm.url" v-bind:src="webm.url" type='video/webm; codecs="vp8, vorbis"'>
   		<source v-if="mp4.url" v-bind:src="mp4.url" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
@@ -17,12 +17,21 @@
     data () {
       return {
         show: false,
+        pixel: __VUE_ENV__ === 'client' ? window.devicePixelRatio : null,
       };
     },
 
     computed: {
       poster () {
         return this.promo.image && this.promo.image .secure_url;
+      },
+
+      width () {
+        return __VUE_ENV__ === 'client' ? this.$el && this.$el.offsetWidth * this.pixel : null;
+      },
+
+      url: function () {
+        return this.poster + (this.width ? `?w=${this.width}` : '');
       },
 
       webm () {
