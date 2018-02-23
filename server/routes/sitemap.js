7f36a7542e18267cac1e29bb92f51ddfbdbd13c1
Vladislav Poznyak <vladislav.poznyak@skywindgroup.com>
2018-02-23 18:06:09 +0300

    Add debugger & sitemap

--- server/routes/sitemap.js
+++ server/routes/sitemap.js
@@ -1,7 +1,17 @@
-const sitemap = require('keystone-express-sitemap');
+const path = require('path');
+const sitemap = require('sitemap-generator');
+
+const generator = sitemap('http://vessna.wedding', {
+  stripQuerystring: false,
+  filepath: path.join(process.cwd(), 'public/sitemap.xml'),
+});
 
 module.exports = app => {
   app.get('/sitemap.xml', (req, res) => {
-    sitemap.create(keystone, req, res);
+    generator.on('done', () => {
+      res.redirect('/public/sitemap.xml');
+    });
+
+    generator.start();
   });
-}
+};
