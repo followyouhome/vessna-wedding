4ccff2b18daea223777c29cfb1838ea74c3cb155
Vladislav Poznyak <vladislav.poznyak@skywindgroup.com>
2018-11-11 03:33:55 +0300

    Redesign desktop global menu

--- src/components/global/global-navigation.vue
+++ src/components/global/global-navigation.vue
@@ -1,22 +1,20 @@
 <template>
-  <nav class="navigation">
-    <ul class="navigation__main-list" itemscope="" itemtype="https://www.schema.org/SiteNavigationElement">
-      <li class="navigation__main-list__item" v-for="item in items">
-        <router-link class="navigation__main-list__item__link" v-bind:to="{ name: item.route || null, path: item.path || null }">
-          <span class="navigation__main-text" itemprop="name">{{ item.label }}</span>
-          <span class="navigation__icon icon-bars" v-if="item.items"></span>
+  <nav class="global-navigation">
+    <ul class="global-navigation__main-list" itemscope="" itemtype="https://www.schema.org/SiteNavigationElement">
+      <li class="global-navigation__main-list__item" v-for="item in items">
+        <router-link class="global-navigation__main-list__item__link" v-bind:to="getRoute(item)">
+          <span class="global-navigation__text" itemprop="name">{{ item.label }}</span>
+          <span class="global-navigation__icon icon-bars" v-if="item.items"></span>
         </router-link>
-        <div class="navigation__bar" v-if="item.items">
-          <ul class="navigation__secondary-list">
-            <li class="navigation__secondary-list__item" v-for="item in item.items">
-              <router-link class="navigation__secondary-list__item__link" v-bind:to="{ name: item.route || null, path: item.path || null, params: item.params || null }">
-                <span class="navigation__secondary-text">{{ item.name }}</span>
-              </router-link>
-            </li>
-          </ul>
-        </div>
+        <ul class="global-navigation__secondary-list" v-if="item.items">
+          <li class="global-navigation__secondary-list__item" v-for="item in item.items">
+            <router-link class="global-navigation__secondary-list__item__link" v-bind:to="getRoute(item)">
+              <span class="global-navigation__text">{{ item.name }}</span>
+            </router-link>
+          </li>
+        </ul>
       </li>
-      <li class="navigation__main-list__item">
+      <!-- <li class="navigation__main-list__item">
         <div class="navigation__main-list__item__link">
           <span class="navigation__main-text" itemprop="name">Для клиентов</span>
           <span class="navigation__icon icon-bars"></span>
@@ -28,11 +26,11 @@
                 <span class="navigation__secondary-text">Войти, чтобы видеть цены</span>
               </div>
             </li>
-            <!-- <li class="navigation__secondary-list__item" v-if="!user">
+            <li class="navigation__secondary-list__item" v-if="!user">
               <div class="navigation__secondary-list__item__link" @click="signup">
                 <span class="navigation__secondary-text">Зарегистрироваться</span>
               </div>
-            </li> -->
+            </li>
             <li class="navigation__secondary-list__item" v-if="user">
               <div class="navigation__secondary-list__item__link" @click="logout">
                 <span class="navigation__secondary-text">Выход</span>
@@ -50,14 +48,14 @@
             </li>
           </ul>
         </div>
-      </li>
+      </li> -->
     </ul>
   </nav>
 </template>
 
 <script>
   export default {
-    name: 'module-nav',
+    name: 'GlobalNavigation',
 
     props: ['navigation'],
 
@@ -91,369 +89,115 @@
       logout () {
         this.$store.dispatch('logout');
       },
+
+      getRoute (item) {
+        return {
+          path: item.path || null,
+          name: item.route || null,
+          params: item.params || null,
+        };
+      },
     },
   };
 </script>
 
-<style lang="scss" scoped>
-  $navigationHeight: 66px;
-  $navigationMain: 1000;
-  $navigationSecondary: $navigationMain + 1;
-
-  .navigation {
-    position: sticky;
-    height: $navigationHeight;
-    top: 0;
-    border-bottom: $gray1 solid 1px;
-    background-color: $white;
-    z-index: $navigationMain;
-
-    @media #{$tablet} {
-      height: $navigationHeight * 2 / 3;
+<style lang="scss">
+  @keyframes fade {
+    0% {
+      opacity: 1;
+      transform: translateY(0px);
     }
 
-    @media #{$phablet} {
-      height: auto;
+    49% {
+      opacity: 0;
+      transform: translateY(10px);
+
     }
 
-    &:before, &:after {                             //For stickify
-      content: '';
-      display: table;
+    51% {
+      opacity: 0;
+      transform: translateY(-10px);
     }
 
-    &.navigation--mobile {
-      height: 40px;
-      text-align: center;
-
-      &:before {
-        content: "\e909";
-        display: inline-block;
-        position: absolute;
-        left: 10px;
-
-        font-family: 'icomoon' !important;
-        font-size: 20px;
-        line-height: 40px;
-      }
-
-      &:after {
-        content: 'Vessna';
-        display: inline-block;
-
-        text-transform: uppercase;
-        line-height: 40px;
-        font-size: 30px;
-      }
+    100% {
+      opacity: 1;
+      transform: translateY(0px);
     }
   }
 
-  .navigation__main-list {
-    margin: 0;
-    height: 100%;
-    text-align: center;
-
-    .navigation--mobile & {
-      position: fixed;
-      overflow: auto;
-      width: 80%;
-      top: 0;
-
-      transition: left .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-
-      @media #{$large} {
-        width: 25%;
-      }
-
-      @media #{$medium} {
-        width: 30%;
-      }
-
-      @media #{$tablet} {
-        width: 50%;
-      }
-
-      @media #{$phablet} {
-        width: 70%;
-      }
-    }
-
-    .navigation--mobile.navigation--swipe-opened & {
-      left: 0;
-    }
-
-    .navigation--mobile.navigation--swipe-closed & {
-      left: -80%;
-
-      @media #{$large} {
-        left: -25%;
-      }
-
-      @media #{$medium} {
-        left: -30%;
-      }
-
-      @media #{$tablet} {
-        left: -50%;
-      }
-
-      @media #{$phablet} {
-        left: -70%;
-      }
-    }
-  }
-
-  .navigation__main-list__item {
-    display: inline-block;
+  .global-navigation {
     position: relative;
-    max-width: 20%;
-    height: 100%;
-
-    @media #{$phablet} {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex-direction: column;
-      max-width: none;
-      min-height: 30px;
-      height: auto;
-    }
-
-    .navigation--mobile & {
-      display: block;
-      border-bottom: 1px solid $gray1;
-      max-width: none;
-      height: auto;
-    }
+    background: $white;
   }
 
-  .navigation__main-list__item__link {
-    display: flex;
+  .global-navigation__main-list {
+    height: 60px;
+    text-align: center;
+  }
+
+  .global-navigation__main-list__item {
+    display: inline-block;
+    margin: 0 0.5rem;
+    height: 100%;
+    list-style: none;
+  }
+
+  .global-navigation__main-list__item__link {
+    display: inline-flex;
     align-items: center;
     justify-content: center;
-    position: relative;
+    padding: 0 0.5rem;
     height: 100%;
-    min-width: 70px;
-    padding: 0 13px;
+    color: $dark5;
 
-    transition: all 0.1s linear;
-
-    color: $dark;
-    text-decoration: none;
-    white-space: nowrap;
-
-    @media #{$phablet} {
-      min-height: 30px;
-      height: 100%;
-      width: 100%;
-      line-height: $navigationHeight * 2 / 3;
-    }
-
-    @media #{$mobile} {
-      padding-right: 25px;
-    }
-
-    .navigation--mobile & {
-      height: auto;
-      line-height: $navigationHeight * 2 / 3;
-    }
-
-    // &:after {
-    //  content: '';
-    //  position: absolute;
-    //  display: block;
-    //  margin: 0;
-    //  width: 100%;
-    //  left: 0;
-    //  top: 0;
-    //  height: 0;
-    //  box-sizing: border-box;
-
-    //  transition: all 0.1s linear;
-
-    //  @media @phablet {
-    //    display: none;
-    //  }
-    // }
-
-    &:active {
-      transform: translateY(2px);
-    }
-
-    &:focus {
-      outline: none;
-      color: $dark;
-      text-decoration: none;
-    }
-
-    .navigation__main-list__item:hover & {
-      background-color: $navigationHoverBackground;
-      color: $white;
-      text-decoration: none;
-
-      // &:after {
-      //  height: 10px;
-      //  margin-top: -10px;
-      //  background-color: @navigationHoverBackground;
-      // }
-    }
-
-    .navigation__main-list__item--show-menu & {
-      color: $white;
-      background: $dark3;
-    }
-
-    .navigation--mobile & {
-      display: block;
-
-      text-align: left;
-      line-height: 40px;
+    .global-navigation__main-list__item:hover & {
+      animation: 0.2s fade;
+      color: $black;
     }
   }
 
-  .navigation__bar {
+  .global-navigation__secondary-list {
     display: none;
     position: absolute;
-    min-width: 100%;
-    right: 0;
+    height: 60px;
+    width: 100%;
     left: 0;
-    z-index: $navigationSecondary;
+    top: 100%;
+    text-align: center;
+    background: $white;
+    border-top: 1px solid $gray1;
 
-    box-shadow: 0 1px 3px rgba(0,0,0,.06),0 1px 2px rgba(0,0,0,.06);
-    background: $glass2;
-
-    .navigation--mobile & {
-      position: static;
-    }
-
-    @media #{$phablet} {
-      position: static;
-    }
-
-    .navigation__main-list__item:hover & {
-      display: table;
-    }
-
-
-    .navigation__main-list__item--show-menu & {
+    .global-navigation__main-list__item:hover & {
       display: block;
-      background: $white !important;
+
     }
   }
 
-  .navigation__secondary-list {
-    z-index: $navigationSecondary;
-
-    transition: all 0.1s linear;
-
-    @media #{$tablet} {
-      max-width: none;
-      float: none;
-    }
-
-    .navigation--mobile & {
-      position: relative;
-      max-width: none;
-      float: none;
-    }
-  }
-
-  .navigation__secondary-list__item {
-    display: block;
-
-    @media #{$tablet} {
-      display: inline-block;
-    }
-
-    .navigation--mobile & {
-      display: inline-block;
-    }
-  }
-
-  .navigation__secondary-list__item__link {
-    display: block;
-    margin: 5px 10px;
-    padding: 5px;
+  .global-navigation__secondary-list__item {
+    display: inline-block;
+    margin: 0 0.5rem;
     height: 100%;
-    border-radius: 1px;
+    list-style: none;
+  }
 
-    text-align: left;
-    color: $dark0;
-
-    transition: all 0.1s linear;
-    cursor: pointer;
-    // color: @white;
-
-    // .vertical_align;
-
-    &:focus {
-      outline: none;
-      color: $white;
-      text-decoration: none;
-    }
-
-    &:visited {
-      background: $dark2;
-      color: $dark5;
-    }
+  .global-navigation__secondary-list__item__link {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 0.5rem;
+    height: 100%;
+    color: $dark5;
 
     &:hover {
-      background: $dark3;
-      color: $white;
-
-      text-decoration: none;
-    }
-
-    &:active {
-      transform: translateY(2px);
-    }
-
-    .navigation--mobile & {
-      color: $dark5;
+      color: $black;
     }
   }
 
-  .navigation__promo {
-    position: relative;
-    float: right;
-    width: 40%;
-    height: auto;
-    overflow: hidden;
-
-    background: $white;
-    box-shadow: 0 1px 3px rgba(0,0,0,.06),0 1px 2px rgba(0,0,0,.06);
-
-    & + .navigation__secondary-list {                     //Dirty hack for navigation sublist
-      display: none;
-    }
-
-    @media #{$tablet} {
-        display: block;
-        float: none;
-        width: 300px;
-        margin: 20px auto;
-    }
-
-    @media #{$phablet} {
-        width: 90%;
-    }
-
-    .navigation--mobile & {
-      display: block;
-        float: none;
-        width: 90%;
-        margin: 20px auto;
-    }
-  }
-
-  .navigation__promo-image-wrapper {
-    margin: 0 0 10px;
-  }
-
-  .navigation__promo-headline {
-    margin: 10px 0;
-  }
-
-  .navigation__promo-subheadline {
-    margin: 10px 0;
+  .global-navigation__text {
+    font: 1.2rem/1 $RistrettoProMedium;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    color: inherit;
+    transition: color linear 0.2s;
   }
 </style>
