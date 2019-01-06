971318ec790e2c7ad4f873a4a94e0dbfd22e0545
Vladislav Poznyak <izvergsklizmoy@gmail.com>
2019-01-06 23:01:15 +0300

    Move mongo config to env params

--- config/keystone.config.js
+++ config/keystone.config.js
@@ -5,7 +5,7 @@ const config = require('../config');
 module.exports = {
   'name': config.title,
   'favicon': '../public/logo-48.png',
-  'mongo': 'mongodb://localhost:27017/' + pkg.name,
+  'mongo': process.env.MONGODB_URI,
   'updates': path.resolve(__dirname, '../server/keystone/updates'),
   'auto update': true,
   'port': config.port,
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const keystone = require('keystone');
 const types = require('./keystone/models');
 
+require('dotenv').config();
+
 const pattern = '<script src="<%= adminPath %>/js/admin.js"></script>\n';
 const template = fs.readFileSync('node_modules/keystone/admin/server/templates/index.html', 'utf-8');
 const updated = template.replace(pattern, pattern + '<script src="/public/scripts/hooks.js"></script>');
