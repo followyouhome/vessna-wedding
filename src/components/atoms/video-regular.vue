0d65ce7df7ffbacb182891bbed9e93c70c96eaec
Vladislav Pozniak <izvergsklizmoy@gmail.com>
2019-03-19 01:06:57 +0300

    Remove full size of video poster

--- src/components/atoms/video-regular.vue
+++ src/components/atoms/video-regular.vue
@@ -1,5 +1,5 @@
 <template>
-  <figure class="video-regular" :style="`background-image: url(${url});`">
+  <figure class="video-regular" :style="style" ref="wrapper">
   	<video class="video-regular__video" autoplay loop muted preload="none" ref="video" v-show="show" v-bind:src="webm.url">
   		<source v-if="webm.url" v-bind:src="webm.url" type='video/webm; codecs="vp8, vorbis"'>
   		<source v-if="mp4.url" v-bind:src="mp4.url" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
@@ -23,19 +23,23 @@
 
     computed: {
       poster () {
-        return this.promo.image && this.promo.image.secure_url;
+        return this.promo.image && this.promo.image.url;
       },
 
       width () {
         return __VUE_ENV__ === 'client' ? this.$el && this.$el.offsetWidth * this.pixel : null;
       },
 
-      url: function () {
+      url () {
         if (this.poster) {
           return this.poster + (this.width ? `?w=${this.width}` : '');
         } else {
           return '';
         }
+      },
+
+      style () {
+        return __VUE_ENV__ === 'client' ? `background-image: url(${this.url});` : '';
       },
 
       webm () {
@@ -53,6 +57,7 @@
 
     mounted () {
       if (__VUE_ENV__ === 'client') {
+        this.$refs.wrapper.setAttribute('style', this.style);
         setTimeout (() => {
           this.show = true;
 
