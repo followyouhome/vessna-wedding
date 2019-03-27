eda034bb8b55cc2a010b8598f198ffb3356f53e7
Vladislav Pozniak <vladislav.pozniak@nitka.com>
2019-03-27 15:53:25 +0300

    Move b2b routes to global namespace

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,15 @@
-import axios from 'axios';
 import Vue from 'vue';
 import Router from 'vue-router';
-import config from '../../config';
-import keystoneConfig from '../../config/keystone.config';
 
 import * as ROUTES from '../../config/constants';
 
 Vue.use(Router);
 
-import b2b from '@/router/routes/b2b.js';
 import user from '@/router/routes/user.js';
 import news from '@/router/routes/news.js';
 import collection from '@/router/routes/collection.js';
 
-export function createRouter (store) {
+export function createRouter () {
   return new Router({
     mode: 'history',
     routes: [
@@ -22,10 +18,19 @@ export function createRouter (store) {
         path: '/',
         component: () => import(/* webpackChunkName: "page-homepage" */ '../components/pages/page-homepage.vue'),
       },
+      {
+        name: 'page-contact',
+        path: '/contact',
+        component: () => import(/* webpackChunkName: "page-contact" */ '@/components/pages/page-contact.vue'),
+      },
+      {
+        name: 'page-about',
+        path: '/about',
+        component: () => import(/* webpackChunkName: "page-about" */ '@/components/pages/page-about.vue'),
+      },
       ...collection,
       ...user,
       ...news,
-      ...b2b,
     ],
   });
 }
