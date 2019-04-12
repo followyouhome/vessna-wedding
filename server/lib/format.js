dd255f6057a31c9c3b4c266b39ce771350dfcbd5
Vladislav Pozniak <vladislav.pozniak@nitka.com>
2019-04-12 14:51:17 +0300

    Add email formatter

--- server/lib/format.js
+++ server/lib/format.js
@@ -1,8 +1,28 @@
-module.exports.subscriberFormat = function (email) {
+module.exports.subscriberFormat = email => {
   return {
     email_address: email,
     email_type: 'html',
     language: 'ru',
     status: 'subscribed',
+  };
+};
+
+module.exports.mailFormat = body => {
+  return {
+    to: null,
+    from: 'Vessna Робот <robot@vessna.wedding>',
+    subject: 'Новое сообщение с сайта Vessna-Wedding',
+    text: `
+      Имя: ${body.name || 'Нет'}
+      Тема: ${body.topic || 'Нет'}
+      Город: ${body.city || 'Нет'}
+      Email: ${body.email || 'Нет'}
+      Страна: ${body.country || 'Нет'}
+      Телефон: ${body.phone || 'Нет'}
+      Сообщение: ${body.message || 'Нет'}
+      Обратный звонок: ${body.callback || 'Нет'}
+      Получить каталог: ${body.lookbook || 'Нет'}
+      Подписаться на рассылку: ${body.subscribe || 'Нет'}
+    `,
   };
 };
