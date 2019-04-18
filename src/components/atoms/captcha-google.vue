9f0ff39157cda23047546d5d6cf3897439bd6eca
Vladislav Pozniak <vladislav.pozniak@nitka.com>
2019-04-18 22:21:05 +0300

    Fix recaptcha styles according to new forms

--- src/components/atoms/captcha-google.vue
+++ src/components/atoms/captcha-google.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+  const CAPTCHA_TIMEOUT = 5000;
+
   export default {
     name: 'CaptchaGoogle',
 
@@ -16,8 +18,12 @@
     mounted () {
       if (__VUE_ENV__ === 'client') {
         if (!document.getElementById('g-recaptcha')) {
-          const body = document.body;
           const script = document.createElement('script');
+          const timeout = setTimeout(() => {
+            this.$emit('failure');
+          }, CAPTCHA_TIMEOUT);
+
+          this.$emit('init');
 
           script.setAttribute('id', 'g-recaptcha');
           script.setAttribute('src', this.script);
@@ -25,6 +31,7 @@
           document.head.appendChild(script);
 
           window.gcallback = () => {
+            clearTimeout(timeout);
             this.$emit('success');
           };
         }
@@ -35,7 +42,26 @@
 
 <style lang="scss">
   .g-recaptcha {
+    position: relative;
     margin: auto;
     width: 304px;
+
+    & > div > div {
+      position: relative;
+      height: 100%;
+      width: 100%;
+    }
+
+    & iframe {
+      margin: auto;
+      bottom: 0;
+      right: 0;
+      left: 0;
+      top: 0;
+    }
   }
 </style>
+
+<story group="Atoms" name="Captcha Google">
+  <captcha-google/>
+</story>
