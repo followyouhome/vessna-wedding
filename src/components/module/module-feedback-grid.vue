1e4ee26ba6537d2696bbc4d424361935ba05facc
Vladislav Pozniak <vladislav.pozniak@nitka.com>
2019-04-19 19:25:28 +0300

    Create feedback grid with story

--- src/components/module/module-feedback-grid.vue
+++ src/components/module/module-feedback-grid.vue
@@ -1,39 +1,42 @@
 <template>
-	<article class="module module-feedback-greed" v-if="feedbacks && feedbacks.links && feedbacks.links.length">
-		<div class="module__wrapper">
-			<div class="images-wrapper">
-				<promo-feedback v-for="link in links" :feedback="link" :key="link.name"/>
-			</div>
-			<div class="text-wrapper">
-				<div class="border">
-					<div class="headline">
-						{{headline}}
-					</div>
-				</div>
-			</div>
+	<b-container class="module module-feedback-greed">
+		<div class="module__wrapper module-feedback-greed__wrapper">
+			<promo-label class="module-feedback-greed__promo" :item="label"/>
+			<promo-feedback class="module-feedback-greed__promo" v-for="feedback in limitedFeedbacks" :item="feedback" :key="feedback.name"/>
 		</div>
-	</article>
+	</b-container>
 </template>
 
 <script>
-	import PromoFeedback from '../promo/promo-feedback.vue';
+	import PromoLabel from '@/components/promo/promo-label.vue';
+	import PromoFeedback from '@/components/promo/promo-feedback.vue';
 
 	export default {
-		name: 'module-feedback-grid',
+		name: 'ModuleFeedbackGrid',
 
 		props: ['feedbacks'],
 
 		components: {
+			'promo-label': PromoLabel,
 			'promo-feedback': PromoFeedback,
 		},
 
-		computed: {
-			headline () {
-				return this.feedbacks.headline;
-			},
+		data () {
+			return {
+				label: {
+					headline: '#vessna2018',
+		      copy: 'К открытию сезона 2019',
+		      button: {
+		        text: 'Открыть Instagram',
+		        href: '#',
+		      },
+				},
+			};
+		},
 
-			links () {
-				return this.feedbacks.links;
+		computed: {
+			limitedFeedbacks () {
+				return this.feedbacks.slice(0, 5);
 			},
 		},
 	};
@@ -46,114 +49,114 @@
 		@media #{$mobile} {
 			display: none;
 		}
+	}
 
-		.promo {
+	.module-feedback-greed__wrapper {
+		display: flex;
+		flex-wrap: wrap;
+		position: relative;
+	}
+
+	.module-feedback-greed__promo {
+		&:nth-child(1) {
+			width: 50%;
+			align-self: flex-end;
+		}
+
+		&:nth-child(2) {
+			width: 50%;
+			align-self: flex-end;
+		}
+
+		&:nth-child(3) {
+			width: 15%;
+		}
+
+		&:nth-child(4) {
+			width: 35%;
+		}
+
+		&:nth-child(5) {
+			width: 30%;
+		}
+
+		&:nth-child(6) {
+			width: 20%;
+		}
+
+		@media #{$tablet} {
 			&:nth-child(1) {
-				width: 25%;
+				width: 33%;
 				align-self: flex-end;
 			}
 
 			&:nth-child(2) {
-				width: 25%;
+				width: 67%;
 				align-self: flex-end;
 			}
 
 			&:nth-child(3) {
-				width: 50%;
-				align-self: flex-end;
+				width: 25%;
+				align-self: flex-start;
 			}
 
 			&:nth-child(4) {
-				width: 15%;
+				width: 50%;
 			}
 
 			&:nth-child(5) {
-				width: 35%;
+				width: 25%;
 			}
 
 			&:nth-child(6) {
-				width: 30%;
+				display: none;
 			}
 
 			&:nth-child(7) {
-				width: 20%;
-			}
-
-			@media #{$tablet} {
-				&:nth-child(1) {
-					width: 33%;
-					align-self: flex-end;
-				}
-
-				&:nth-child(2) {
-					width: 67%;
-					align-self: flex-end;
-				}
-
-				&:nth-child(3) {
-					width: 25%;
-					align-self: flex-start;
-				}
-
-				&:nth-child(4) {
-					width: 50%;
-				}
-
-				&:nth-child(5) {
-					width: 25%;
-				}
-
-				&:nth-child(6) {
-					display: none;
-				}
-
-				&:nth-child(7) {
-					display: none;
-				}
+				display: none;
 			}
 		}
-	}
-
-	.module__wrapper {
-		position: relative;
-	}
-
-	.text-wrapper {
-		position: absolute;
-		bottom: 0;
-		right: 0;
-		left: 0;
-		top: 0;
-		margin: auto;
-		z-index: 501;
-		padding: 20px;
-		box-sizing: border-box;
-		height: 350px;
-		width: 460px;
-		background: $glass3;
-
-		@media #{$tablet} {
-			height: 250px;
-			width: 350px;
-		}
-	}
-
-	.images-wrapper {
-		display: flex;
-		flex-wrap: wrap;
-	}
-
-	.border {
-		height: 100%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		border: 1px solid $gray3;
-	}
-
-	.headline {
-		font-family: 'Akzidenz Grotesk';
-		font-size: 2.5rem;
-		color: $black2;
 	}
 </style>
+
+<story group="Modules" name="Feedback Grid">
+	<module-feedback-grid
+		:feedbacks="[{
+			image: {
+				height: 336,
+				width: 269,
+				url: 'https://marymaximca.cdn.speedyrails.net/media/catalog/product/cache/83cda418bea4fb032eada316ef024fcb/f/p/fp9458_rev_1.jpg'
+			}
+		}, {
+			image: {
+				height: 336,
+				width: 269,
+				url: 'http://timelesslinen.com/wp-content/uploads/2018/04/Linen-Pinafore-apron-Kids-Gold.jpg'
+			}
+		}, {
+			image: {
+				height: 336,
+				width: 269,
+				url: 'https://i.etsystatic.com/9281366/c/1019/809/0/329/il/4e8b76/1223235278/il_340x270.1223235278_93v0.jpg'
+			}
+		}, {
+			image: {
+				height: 336,
+				width: 269,
+				url: 'https://i.pinimg.com/236x/f5/2a/ed/f52aed5c13c3f62aea826bf7b71fcad1--blue-wedding-dresses-blue-weddings.jpg'
+			}
+		}, {
+			image: {
+				height: 336,
+				width: 269,
+				url: 'https://st2.depositphotos.com/3647147/11127/i/950/depositphotos_111275726-stock-photo-beauty-woman-clever-smart-girl.jpg'
+			}
+		}, {
+			image: {
+				height: 336,
+				width: 269,
+				url: 'https://marymaximca.cdn.speedyrails.net/media/catalog/product/cache/83cda418bea4fb032eada316ef024fcb/f/p/fp9458_rev_1.jpg'
+			}
+		}]"
+	/>
+</story>
