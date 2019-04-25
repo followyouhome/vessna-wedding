c22ce70a375d2460de04f8966c48e08fa86140a6
Vladislav Pozniak <vladislav.pozniak@nitka.com>
2019-04-26 02:04:29 +0300

    Allow folders for image proxy

--- server/routes/proxy.js
+++ server/routes/proxy.js
@@ -1,15 +1,23 @@
 const http = require('http');
 
 module.exports = app => {
-  app.use('/images/:filename', function (req, res) {
+  app.use('/images/:folder?/:filename?', function (req, res) {
     const cloudinary = 'http://res.cloudinary.com/vessna/image/upload';
     let query = '';
+    let url = '';
 
     if (req.query.w) {
       query += `w_${req.query.w}`;
     }
 
-    const image = http.request(`${cloudinary}/${query}/${req.params.filename}`, newRes => {
+    url = `${cloudinary}/${query}/${req.params.folder}`;
+
+    if (req.params.filename) {
+      url += `/${req.params.filename}`;
+    }
+
+
+    const image = http.request(url, newRes => {
         newRes.headers['Cache-Control'] = 'public, max-age=31557600';
 
         res.writeHead(newRes.statusCode, newRes.headers);
